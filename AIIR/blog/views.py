a0ecabddf7c2ed69d8e92b1590d8278c8ad47052
KostiukovAleksey <30317283+KostiukovAleksey@users.noreply.github.com>
2019-05-09 17:00:06 +0200

    queue v1

--- AIIR/blog/views.py
+++ AIIR/blog/views.py
@@ -1,7 +1,6 @@
 from PIL import Image
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from rest_framework.decorators import api_view
-from django.http import HttpResponse, HttpResponseRedirect
 from rest_framework.response import Response
 from rest_framework import status
 import paramiko
@@ -9,11 +8,10 @@ from django.contrib.auth.models import User
 from .models import Request
 from .models import Algorithm
 from django.contrib.auth import authenticate, login as auth_login
-from django.core import serializers
-import json
 from datetime import datetime
-from django.core.serializers.json import DjangoJSONEncoder
 import time
+import threading
+
 
 
 def home(request):
@@ -62,24 +60,8 @@ def task_manager(request):
     return render(request, 'blog/task_manager.html', {'data': data, 'user_id': user.id})
 
 
-progress1 = 0
-progress2 = 0
-progress3 = 0
-img_url = ''
-
-
 @api_view(['POST'])
 def execute_algorithm(request):
-    global progress1
-    global progress2
-    global progress3
-    global img_url
-    
-    progress1 = 0
-    progress2 = 0
-    progress3 = 0
-    img_url = ''
-    
     first_param = request.data['firstParam']
     second_param = request.data['secondParam']
     third_param = request.data['thirdParam']
@@ -88,58 +70,76 @@ def execute_algorithm(request):
     user = User.objects.get(id=user_id)
     algorithm = Algorithm.objects.get(algorithm_name="mandelbrot")
     new_task = Request.objects.create(user=user, algorithm=algorithm, date=datetime.now(), parameter1=first_param,
-                                      parameter2=second_param, parameter3=third_param, status="In queue")
-    idOfDoneFractal = new_task.id
+                                      parameter2=second_param, parameter3=third_param, status="In queue", progress1=0,
+                                      progress2=0, progress3=0, image_url="../../static/images/fractal_square.jpg")
+    threading.Thread(target=send_task_to_master).start()
+    return Response(new_task.id, status.HTTP_200_OK)
 
-    host = '192.168.0.109'
-    port = 22
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=host, username="vm1", password="wlodek", port=port)
-    stdin, stdout, stderr = client.exec_command('./run.sh ' + first_param
-                                                + ' ' + second_param + ' ' + third_param)
-    stdin.close()
-    for line in iter(lambda: stdout.readline(2048), ""):
-        if '::' in line:
-            array = line.split(":: ")
-            info = array[0]
-            progress = array[1].replace('\n', '')
-            if info == "Master":
-                progress1 = progress
-            elif info == "Slave1":
-                progress2 = progress
-            elif info == "Slave2":
-                progress3 = progress
 
-    client.close()
-    client.close()
-    transport = paramiko.Transport(host, port)
-    transport.connect(username='vm1', password='wlodek')
-    sftp = paramiko.SFTPClient.from_transport(transport)
+master_busy = False
 
-    remotepath = '/home/vm1/out.png'
-    localpath = 'out.png'
-    sftp.get(remotepath, localpath)
-    sftp.close()
-    transport.close()
 
-    # for k in range(10):
-    #     time.sleep(1)
-    #     progress1=(k+1) * 0.1
-    #     progress2 = (k + 1) * 0.1
-    #     progress3 = (k + 1) * 0.1
+def send_task_to_master():
+    global master_busy
 
-    image = Image.open('out.png')
-    bg = Image.new('RGB', image.size, (255, 255, 255))
-    bg.paste(image, (0, 0), image)
-    bg.save("./blog/static/images/img" + str(new_task_id) + ".jpg", quality=95)
+    while master_busy:
+        time.sleep(10)
+    master_busy = True
+    for new_task in Request.objects.select_related().filter(status="In queue"):
+        host = '192.168.0.109'
+        port = 22
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname=host, username="vm1", password="wlodek", port=port)
+        stdin, stdout, stderr = client.exec_command('./run.sh ' + new_task.first_param
+                                                    + ' ' + new_task.second_param + ' ' + new_task.third_param)
+        stdin.close()
+        progress1 = 0
+        progress2 = 0
+        progress3 = 0
+        count = 0
+        for line in iter(lambda: stdout.readline(2048), ""):
+            if '::' in line:
+                count += 1
+                array = line.split(":: ")
+                info = array[0]
+                progress = array[1].replace('\n', '')
+                if info == "Master":
+                    progress1 = progress
+                elif info == "Slave1":
+                    progress2 = progress
+                elif info == "Slave2":
+                    progress3 = progress
+                if count % 50:
+                    new_task.progress1 = progress1
+                    new_task.progress2 = progress2
+                    new_task.progress3 = progress3
+                    new_task.save()
+        client.close()
+        client.close()
+        transport = paramiko.Transport(host, port)
+        transport.connect(username='vm1', password='wlodek')
+        sftp = paramiko.SFTPClient.from_transport(transport)
 
-    new_task = Request.objects.get(id=idOfDoneFractal)
-    new_task.image_url = "../../static/images/img" + str(idOfDoneFractal) + ".jpg"
-    new_task.status = "Done"
-    new_task.save()
-    img_url = new_task.image_url
-    return render(request, 'blog/task_manager.html', {'task': new_task, 'user_id': user_id})
+        remotepath = '/home/vm1/out.png'
+        localpath = 'out.png'
+        sftp.get(remotepath, localpath)
+        sftp.close()
+        transport.close()
+
+        image = Image.open('out.png')
+        bg = Image.new('RGB', image.size, (255, 255, 255))
+        bg.paste(image, (0, 0), image)
+        bg.save("./blog/static/images/img" + str(new_task.id) + ".jpg", quality=95)
+
+        new_task.progress1 = progress1
+        new_task.progress2 = progress2
+        new_task.progress3 = progress3
+        new_task.image_url = "../../static/images/img" + str(new_task.id) + ".jpg"
+        new_task.status = "Done"
+        new_task.save()
+    master_busy = False
+    return
 
 
 @api_view(['POST'])
@@ -158,20 +158,12 @@ def reset(request):
 
 @api_view(['GET'])
 def progress_bar(request):
-    global progress1
-    global progress2
-    global progress3
-    global img_url
-    data = [float(progress1) * 100, float(progress2) * 100, float(progress3) * 100, img_url]
-    print(data)
+    task_id = int(request.GET['task_id'])
+    task_from_db = Request.objects.get(id=task_id)
+    data = [float(task_from_db.progress1) * 100, float(task_from_db.progress2) * 100, float(task_from_db.progress3) * 100,
+            task_from_db.image_url]
     return Response(data, status.HTTP_200_OK)
 
-#Default parameters
-class fakeTask:
-    progress1=0
-    progress2=0
-    progress3=0
-    image_url="../../static/images/fractal_square.jpg"
 
 def task(request):
     user_id = request.GET['user_id']
@@ -179,5 +171,5 @@ def task(request):
         t = Request.objects.get(id=request.GET['task_id'])
         return render(request, 'blog/task.html', {'task': t, 'user_id': user_id})
     else:
-        t = fakeTask()
-        return render(request, 'blog/task.html', {'task': t, 'user_id': user_id})
+        return render(request, 'blog/task.html', {'task': None, 'user_id': user_id})
+
